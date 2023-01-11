75c4decd5eb759e2436fb47c817888f08a26431b
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-01-10 23:09:25 -0600

    Got the local storage save and retrieval working. Worked on populating text fields with saved data, it is not working yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,49 @@
 
 $(function () {
 
-  // This sets the date and the hour. Don't forget to switch the hour back to real-time.
+  let chosenHour;
+  let chosenHourValue;
+  let newOb;
+  let newAr;
+  let saveData = [];
+  let textValue;
+
+  // This sets the date and the hour.
   const time = dayjs();
-  const hour = 12;
-  // const hour = time.format("H");
+  const hour = time.format("H");
   
   // This displays the current date in the header :)
   const currentDay = $("#currentDay").text(time.format("MMMM D, YYYY")).attr("class", "h2")
+
+    // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+
+// This gets the saveData from local storage, and keeps it from nullifying saveData if there's nothing there.
+newAr = JSON.parse(localStorage.getItem("saveData"))
+if( newAr !== null ){
+  saveData = newAr;
+}
 
 // This writes the calendar to the page using jQuery.
 for(i=9;i<18;i++){
 $(".calendar").append(
   $("<div>").attr("id", `hour-${i}`).addClass(`row time-block ${isItTime(i)}`).append(
     $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(amPm(i)), 
-    $("<textarea>").addClass("col-8 col-md-10 description").attr("rows", "3"),
-    $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").append(
+    $("<textarea>").addClass("col-8 col-md-10 description").attr("rows", "3").data("id", `${i}`).text(getText(i)),
+    $("<button>").addClass(`btn saveBtn col-2 col-md-1`).attr("aria-label", "save").append(
       $("<i>").addClass("fas fa-save").attr("aria-hidden", "true"))))}
       
+//What do I need to do when populating the text fields with saved data?
+  //
+
+// This gets the saved text for the loop.
+function getText(i){
+  let textValue = saveData.find(function(value){return value.hour === i});
+  return textValue
+}
+console.log(textValue)
+
 // This tells the for loop whether to say AM or PM.
 function amPm(i){
   if(i<12){
@@ -55,11 +81,25 @@ function isItTime(i){
   // useful when saving the description in local storage?
   //
 
-
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+// This saves the text input to local storage
+// $(".btn").click(function(){
+//   chosenHour = ($(this).prev().data("id"));
+//   chosenHourValue = ($(this).prev().val());
+//   newOb = {
+//     [chosenHour]: chosenHourValue
+//   }
+//   saveData.push(newOb)
+//   localStorage.setItem("saveData", JSON.stringify(saveData))
+// })
+$(".btn").click(function(){
+  chosenHour = ($(this).prev().data("id"));
+  chosenHourValue = ($(this).prev().val());
+  newOb = {
+    hour: chosenHour,
+    value: chosenHourValue
+  }
+  saveData.push(newOb)
+  localStorage.setItem("saveData", JSON.stringify(saveData))
+})
 
 });
